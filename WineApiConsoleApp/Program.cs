8a5878b274e2ea46b7518900a02b66c941faf673
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2011-10-12 00:23:03 +0100

    Got basic task working

---
+++ WineApiConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WineApiConsoleApp.WineDataService;
+
+namespace WineApiConsoleApp
+{
+    internal class Program
+    {
+        private static string API_KEY = "2fd879a5765785c043cc992b550d2bda";
+
+        private static void Main(string[] args)
+        {
+            var wineData = new WineData(new Uri("http://wine.cloudapp.net"));
+            var wineTypes = wineData.WineTypes.AddQueryOption("apikey", API_KEY);
+            foreach (var wineType in wineTypes) {
+                Console.WriteLine(wineType.Name);
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# WineApi
+
+This project demonstrates how to perform a very basic task using the OData service provided by wine.com.
+
+References:
+
+- http://wine.com
+- http://api.wine.com
+- http://www.odata.org/
+- http://msdn.microsoft.com/en-us/library/dd673930.aspx
