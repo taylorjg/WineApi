e9630a0d25978f24bcf6a36ae51c31652b0f4de5
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2012-01-27 09:33:28 +0000

    Added more asserts to a test

--- WineApiUnitTests/CatalogServiceTests.cs
+++ WineApiUnitTests/CatalogServiceTests.cs
@@ -29,6 +29,17 @@ namespace WineApiUnitTests
             Assert.That(catalog, Is.Not.Null);
             Assert.That(catalog.Status.ReturnCode, Is.EqualTo(ReturnCode.Success));
             Assert.That(catalog.Status.Messages, Has.Length.EqualTo(0));
+
+            Assert.That(catalog.Products, Is.Not.Null);
+            Assert.That(catalog.Products.List, Has.Length.EqualTo(10));
+
+            Assert.That(catalog.Products.List[0].Id, Is.EqualTo(108074));
+            Assert.That(catalog.Products.List[0].Name, Is.EqualTo("Duckhorn Napa Merlot 2008"));
+            Assert.That(catalog.Products.List[0].Appellation.Id, Is.EqualTo(2398));
+            Assert.That(catalog.Products.List[0].Appellation.Name, Is.EqualTo("Napa Valley"));
+            Assert.That(catalog.Products.List[0].Appellation.Region.Id, Is.EqualTo(101));
+            Assert.That(catalog.Products.List[0].Appellation.Region.Name, Is.EqualTo("California"));
+            Assert.That(catalog.Products.List[0].Appellation.Region.Area, Is.Null);
         }
     }
 }
